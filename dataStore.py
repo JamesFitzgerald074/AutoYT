7fdd61679b5f7a7d8224679e6c8e8bacb98ac457
JamesFitzgerald074 <jfitzgerald074@abbeycbs.org>
2017-11-14 15:35:29 +0000

    Added Video object dataStore.py

--- dataStore.py
+++ dataStore.py
@@ -31,30 +31,53 @@ class dataStore:
                     )""")
 
     #TODO change to f"" strings in when updated to python3.6
+
+    #TODO add starring catagory
     def addClip(self, movieName='', clipLength=0, director='', releaseDate='',filename=''):
-        self.c.execute("""INSERT INTO clips VALUES ('{}', '{}', '{}', '{}', '{}', '{}')"""
+        self.c.execute("""INSERT INTO clips VALUES (
+                                            '{ID}',
+                                            '{movieName}',
+                                            '{clipLength}',
+                                            '{director}',
+                                            '{releaseDate}',
+                                            '{filename}'
+                                            )"""
             .format("NULL", movieName,clipLength,director,releaseDate, filename))
         conn.commit()
 
     def addVideo(self, videoName='', numberOfItems=0, filename=''):
-        self.c.execute("""INSERT INTO videos VALUES ('{}', '{}', '{}','{}')"""
-            .format("NULL", Name, numberOfItems, filename))
+        self.c.execute("""INSERT INTO videos VALUES (
+                                            '{ID}',
+                                            '{videoName}',
+                                            '{numberOfItems}',
+                                            '{filename}')"""
+            .format("NULL", videoName, numberOfItems, filename))
         conn.commit()
 
     def addVideoClip(self, videoID=0, clipID=0):
-        self.c.execute("""INSERT INTO videoClips VALUES ('{}', '{}', '{}')"""
-            .format("NULL", Name, numberOfItems))
+        self.c.execute("""INSERT INTO videoClips VALUES (
+                            '{videoID}',
+                            '{clipID}'
+                            )"""
+            .format(videoID, clipID))
         conn.commit()
 
+
+    def searchClip(self,movieName=''):
+        self.c.execute("""SELECT * FROM clips WHERE movieName=?""", movieName)
+        return self.c.fetchone()
 
 class Video:
     """class to store the details of each video """
 
-    def __init__(self):
+    def __init__(self, title='', movies=[]):
+        self.title = title
+        self.movies = movies
+        self.clipList = self.buildClipList()
+
+    def buildClipList():
         pass
-
-
-
 test = dataStore()
-test.buildDB()
-test.addClip(movieName='TEST',clipLength=100, director='TEST', releaseDate='TEST',filename='TEST' )
+#test.buildDB()
+#test.addClip(movieName='TEST',clipLength=100, director='TEST', releaseDate='TEST',filename='TEST' )
+print(test.searchClip(movieName="*"))
